f16643a4b5b8cda7afdd0279e67e707f1422d019
Anna <lutomskaya@gmail.com>
2022-03-01 11:38:16 +0300

    исправила замечания

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-var link = document.querySelector('.open-modal-link');
-var modal = document.querySelector('.modal');
-var close = document.querySelector('.modal__close');
-
-link.addEventListener('click', function (event) {
-    event.preventDefault();
-    modal.classList.add('modal__show');
-});
-
-close.addEventListener('click', function (event) {
-    event.preventDefault();
-    modal.classList.remove('modal__show');
-});
-
 var basketLink = document.querySelector('.basket');
 var basketModal = document.querySelector('.popover-basket');
 
@@ -33,3 +19,19 @@ catalogLink.addEventListener("click", function() {
       navMain.classList.remove("main-catalog__opened");
     }
   });
+
+var link = document.querySelector('.open-modal-link');
+var modal = document.querySelector('.modal');
+var close = document.querySelector('.modal__close');
+
+link.addEventListener('click', function (event) {
+    event.preventDefault();
+    modal.classList.add('modal__show');
+});
+
+close.addEventListener('click', function (event) {
+    event.preventDefault();
+    modal.classList.remove('modal__show');
+});
+
+
